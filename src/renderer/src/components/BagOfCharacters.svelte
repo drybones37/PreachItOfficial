ae979420230bf931ecbe356f447e38f95b24c03f
Drybones37 <ashthedash100@gmail.com>
2024-11-23 23:50:17 +0800

    Got the character selection menu to display all the characters in row

--- src/renderer/src/components/BagOfCharacters.svelte
+++ src/renderer/src/components/BagOfCharacters.svelte
@@ -4,26 +4,99 @@
   let bagImgs = 'src/assets/BagOfCharac.webp'
   let bagImgAlt = 'Bag of characters'
   let showAlert = false
+  let selectionRow = [['src/assets/Charac/AfricanBlue.webp',
+    'src/assets/Charac/BruneteGreen.webp',
+    'src/assets/Charac/ManSuitGreen.webp',
+    'src/assets/Charac/AfricanWomanRed.webp',
+    'src/assets/Charac/Brunette.webp',
+    'src/assets/Charac/MonacleMan.webp',
+    'src/assets/Charac/AfricanYellow.webp',],
+    
+    ['src/assets/Charac/AfricanYellow.webp',
+    'src/assets/Charac/BrunetteWomanRed.webp',
+    'src/assets/Charac/MonocleBlack.webp',
+    'src/assets/Charac/AsianGirlYellow.webp',
+    'src/assets/Charac/Casandra.webp',
+    'src/assets/Charac/MonocleYellow.webp',],
+    
+    ['src/assets/Charac/AsianWoman.webp',
+    'src/assets/Charac/DirtyBlondeGreen.webp',
+    'src/assets/Charac/PinoyBlue.webp',
+    'src/assets/Charac/BlondeManGreen.webp',
+    'src/assets/Charac/EbonyBlue.webp',
+    'src/assets/Charac/PinoyMan.webp',],
+    
+    ['src/assets/Charac/BlondeWomanRed.webp',
+    'src/assets/Charac/EbonyWoman.webp',
+    'src/assets/Charac/PinoyYellow.webp',
+    'src/assets/Charac/BlondieBlue.webp',
+    'src/assets/Charac/ManRed.webp',
+    'src/assets/Charac/Rocker.webp']]
 
-  let piecesList = [""]
-  let piecesSelectedList = ['src/assets/Charac/AfricanBlue.webp',      'src/assets/Charac/BruneteGreen.webp',      'src/assets/Charac/ManSuitGreen.webp',
-'src/assets/Charac/AfricanWomanRed.webp',  'src/assets/Charac/Brunette.webp',          'src/assets/Charac/MonacleMan.webp',
-'src/assets/Charac/AfricanYellow.webp',    'src/assets/Charac/BrunetteWomanRed.webp',  'src/assets/Charac/MonocleBlack.webp',
-'src/assets/Charac/AsianGirlYellow.webp',  'src/assets/Charac/Casandra.webp',          'src/assets/Charac/MonocleYellow.webp',
-'src/assets/Charac/AsianWoman.webp',       'src/assets/Charac/DirtyBlondeGreen.webp',  'src/assets/Charac/PinoyBlue.webp',
-'src/assets/Charac/BlondeManGreen.webp',   'src/assets/Charac/EbonyBlue.webp',         'src/assets/Charac/PinoyMan.webp',
-'src/assets/Charac/BlondeWomanRed.webp',   'src/assets/Charac/EbonyWoman.webp',        'src/assets/Charac/PinoyYellow.webp',
-'src/assets/Charac/BlondieBlue.webp',      'src/assets/Charac/ManRed.webp',            'src/assets/Charac/Rocker.webp',]
+  let piecesList = [
+    'src/assets/Charac/AfricanBlue.webp',
+    'src/assets/Charac/BruneteGreen.webp',
+    'src/assets/Charac/ManSuitGreen.webp',
+    'src/assets/Charac/AfricanWomanRed.webp',
+    'src/assets/Charac/Brunette.webp',
+    'src/assets/Charac/MonacleMan.webp',
 
+    'src/assets/Charac/AfricanYellow.webp',
+    'src/assets/Charac/BrunetteWomanRed.webp',
+    'src/assets/Charac/MonocleBlack.webp',
+    'src/assets/Charac/AsianGirlYellow.webp',
+    'src/assets/Charac/Casandra.webp',
+    'src/assets/Charac/MonocleYellow.webp',
 
+    'src/assets/Charac/AsianWoman.webp',
+    'src/assets/Charac/DirtyBlondeGreen.webp',
+    'src/assets/Charac/PinoyBlue.webp',
+    'src/assets/Charac/BlondeManGreen.webp',
+    'src/assets/Charac/EbonyBlue.webp',
+    'src/assets/Charac/PinoyMan.webp',
 
-  function closeBag(){
+    'src/assets/Charac/BlondeWomanRed.webp',
+    'src/assets/Charac/EbonyWoman.webp',
+    'src/assets/Charac/PinoyYellow.webp',
+    'src/assets/Charac/BlondieBlue.webp',
+    'src/assets/Charac/ManRed.webp',
+    'src/assets/Charac/Rocker.webp'
+  ]
+
+  // let piecesSelectedList = [
+  //   'src/assets/Charac/AfricanBlue.webp',
+  //   'src/assets/Charac/BruneteGreen.webp',
+  //   'src/assets/Charac/ManSuitGreen.webp',
+  //   'src/assets/Charac/AfricanWomanRed.webp',
+  //   'src/assets/Charac/Brunette.webp',
+  //   'src/assets/Charac/MonacleMan.webp',
+  //   'src/assets/Charac/AfricanYellow.webp',
+  //   'src/assets/Charac/BrunetteWomanRed.webp',
+  //   'src/assets/Charac/MonocleBlack.webp',
+  //   'src/assets/Charac/AsianGirlYellow.webp',
+  //   'src/assets/Charac/Casandra.webp',
+  //   'src/assets/Charac/MonocleYellow.webp',
+  //   'src/assets/Charac/AsianWoman.webp',
+  //   'src/assets/Charac/DirtyBlondeGreen.webp',
+  //   'src/assets/Charac/PinoyBlue.webp',
+  //   'src/assets/Charac/BlondeManGreen.webp',
+  //   'src/assets/Charac/EbonyBlue.webp',
+  //   'src/assets/Charac/PinoyMan.webp',
+  //   'src/assets/Charac/BlondeWomanRed.webp',
+  //   'src/assets/Charac/EbonyWoman.webp',
+  //   'src/assets/Charac/PinoyYellow.webp',
+  //   'src/assets/Charac/BlondieBlue.webp',
+  //   'src/assets/Charac/ManRed.webp',
+  //   'src/assets/Charac/Rocker.webp'
+  // ]
+
+  function closeBag() {
     showAlert = false
   }
 
-
   function openBag() {
     // Play sound
+    showAlert = true
     bagSound.play()
   }
   // Load the sound effect on mount
@@ -36,12 +109,40 @@
   <div class="custom-alert">
     <div class="alert-content">
       <p>Click the piece you want to add to the board.</p>
-      <wbr/>
-      <div class="alert-buttons">
-        <button on:click={closeBag}>Done</button>
-        <button on:click={closeBag}>Cancel</button>
+      <wbr />
+      <div class="pad">
+        {#each selectionRow as Row}
+        <div class="player-card">
+          <div class="card-content">
+              <div class="image-row">
+                <div class="image-container">
+                  <img src={Row[0]} alt="Character card" class="xp-icon" />
+                </div>
+                <div class="image-container">
+                  <img src={Row[1]} alt="Character card" class="xp-icon" />
+                </div>
+                <div class="image-container">
+                  <img src={Row[2]} alt="Character card" class="xp-icon" />
+                </div>
+                <div class="image-container">
+                  <img src={Row[3]} alt="Character card" class="xp-icon" />
+                </div>
+                <div class="image-container">
+                  <img src={Row[4]} alt="Character card" class="xp-icon" />
+                </div>
+                <div class="image-container">
+                  <img src={Row[5]} alt="Character card" class="xp-icon" />
+                </div>
+              </div>
+          </div>
+        </div>
+        {/each}
       </div>
     </div>
+  </div>
+  <div class="alert-buttons">
+    <button on:click={closeBag}>Done</button>
+    <button on:click={closeBag}>Cancel</button>
   </div>
 {/if}
 
@@ -49,8 +150,9 @@
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div class="card" on:click={openBag}>
   <!-- <div class="image-container"> -->
-  <img src={bagImgs} alt={bagImgAlt} class="ops-card-cover" />
+  <img src={bagImgs} alt={bagImgAlt} />
 </div>
+
 <style>
   img:active {
     transform: scale(0.95);
@@ -71,5 +173,79 @@
     width: auto;
     height: 24vh;
     aspect-ratio: 1/1;
+  }
+
+  .player-card {
+    background-color: #f7f7f7;
+    padding-top: 6px;
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    margin-bottom: 10px;
+  }
+
+  .card-content {
+    padding: 5px;
+  }
+
+  .image-row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+
+  .image-container {
+    position: relative;
+    margin-right: 10px;
+  }
+
+  .xp-icon {
+    width: 35px;
+    height: 50px;
+    border-radius: 4px;
+    border: 1px solid #ccc;
+  }
+
+  .custom-alert {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+  }
+
+  .alert-content {
+    background-color: rgb(36, 35, 35);
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    text-align: center;
+    color: white;
+    min-width: 300px;
+  }
+
+  .alert-buttons {
+    display: flex;
+    justify-content: space-around;
+    margin-top: 20px;
+  }
+
+  .alert-buttons button {
+    padding: 10px 20px;
+    cursor: pointer;
+    background-color: #444;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    transition: background-color 0.2s;
+  }
+
+  .alert-buttons button:hover {
+    background-color: #555;
   }
 </style>
